014e1cbe49b493ff9c08d28014fd667523de5c9d
Appeltysken <Sorokin6203@yandex.ru>
2024-10-08 17:58:13 +0300

    :safety_vest: Added model for User
    - Added fields for the User entity
    - Validation has been added for some fields

--- app/models.py
+++ app/models.py
@@ -1,56 +1,65 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
-from pydantic import BaseModel
-from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
+from enum import Enum
+from pydantic import BaseModel, EmailStr, Field, field_validator, ValidationError
+from datetime import date, datetime
+from typing import Optional
+import re
 
-Base = declarative_base()
 
-class UserModel(Base):
-    __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    password = Column(String)
-    Fname = Column(String)
-    Lname = Column(String)
-    sex = Column(String)
-    email = Column(String)
-    phone = Column(String)
-    createdAt = Column(DateTime, default=func.now())
-    updatedAt = Column(DateTime, default=func.now(), onupdate=func.now())
-    publishedAt = Column(DateTime, default=func.now())
-    reg_date = Column(String)
+class Gender(str, Enum):
+    male = "Мужской"
+    female = "Женский"
 
-class UserBase(BaseModel):
-    username: str
-    password: str
-
-class UserCreate(UserBase):
-    Fname: str
-    Lname: str
-    sex: str
-    email: str
-    phone: str
-    reg_date: str
-
-class User(UserBase):
+class User(BaseModel):
     id: int
-    Fname: str
-    Lname: str
-    sex: str
-    email: str
-    phone: str
-    createdAt: datetime
-    updatedAt: datetime
-    publishedAt: datetime
-    reg_date: str
+    username: str = Field(default=..., description="Логин пользователя, которым выступает номер телефона/адрес эл. почты")
+    password: str = Field(default=..., min_length=8, description="Пароль пользователя, минимальная длина - 8 символов")
+    Fname: str = Field(default=..., min_length=1, max_length=50, description="Имя пользователя, от 1 до 50 символов")
+    Lname: date = Field(default=..., min_length=1, max_length=50, description="Фамилия пользователя, от 1 до 50 символов")
+    sex: str = Field(default=..., description="Пол пользователя")
+    email: EmailStr = Field(default=..., description="Электронная почта пользователя")
+    phone: str = Field(default=..., description="Номер телефона пользователя")
+    sell_history: str = Field(default=..., description="История продаж")
+    buy_history: str = Field(default=..., description="История покупок")
 
-    class Config:
-        orm_mode = True
+    @field_validator('username')
+    @classmethod
+    def validate_username(cls, value: str) -> str:
+        phone = re.sub(r'\D', '', value)
+        
+        if len(phone) == 11 and phone.startswith("8"):
+            return "7" + phone[1:]
 
-class UserBookResponse(BaseModel):
-    data: dict
-    meta: dict
+        elif len(phone) == 11 and phone.startswith("7"):
+            return phone
 
-    class Config:
-        orm_mode = True
+        elif re.match(r'^\S+@\S+\.\S+$', value):
+            return value
+        
+        else:
+            raise ValueError("Username должен быть номером телефона или адресом эл. почты")
+    
+    
+    @field_validator('password')
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        
+        if not re.search(r'[A-Za-z]', value):
+            raise ValueError('Пароль должен содержать хотя бы одну букву')
+        
+        if not re.search(r'\d', value):
+            raise ValueError('Пароль должен содержать хотя бы одну цифру')
+        
+        if not re.search(r'\W', value):
+            raise ValueError('Пароль должен содержать хотя бы один специальный символ')
+        
+        return value
+    
+    @field_validator("phone")
+    @classmethod
+    def validate_phone(cls, values: str) -> str:
+        
+        if not re.match(r'^\+\d{1,15}$', values):
+            raise ValueError('Номер телефона должен начинаться с "+" и содержать от 1 до 15 цифр')
+        
+        return values
