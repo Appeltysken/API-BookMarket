d4f2fea47ba0f8453bbe508ba4aa9d7f0fe7c190
Appeltysken <Sorokin6203@yandex.ru>
2024-10-08 23:14:45 +0300

    :card_file_box: Created connection beyond API and PostgreSQL
    - The old models.py has been redesigned and moved to dict users_book
    - Added migration
    - The database.py was returned and the logic of interaction with the database is prescribed
    - Created config.py to link the .env file and the database

---
+++ app/users_book/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from app.database import Base, str_uniq, int_pk, str_null_true
+from datetime import date
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class User(Base):
+    __tablename__ = "users-book"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    Fname: Mapped[str] = mapped_column(String, nullable=False)
+    Lname: Mapped[str] = mapped_column(String, nullable=False)
+    sex: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    phone: Mapped[str] = mapped_column(String, nullable=True)
+    sell_history: Mapped[str] = mapped_column(String, nullable=True)
+    buy_history: Mapped[str] = mapped_column(String, nullable=True)
+
+    def set_password(self, password: str):
+        self.password = pwd_context.hash(password)
+
+    def verify_password(self, password: str) -> bool:
+        return pwd_context.verify(password, self.password)
+
+    def __str__(self):
+        return (f"{self.__class__.__name__}(id={self.id}, "
+                f"first_name={self.Fname!r},"
+                f" last_name={self.Lname!r})")
+
+    def __repr__(self):
+        return str(self)
