dea4b30eca9640d83ce8a2449edaf31052837855
Appeltysken <Sorokin6203@yandex.ru>
2024-10-14 23:45:35 +0300

    :bricks: Added the output of a new router

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 import os
 from app.users.router import router as router_users
 from app.orders.router import router as router_orders
+from app.roles.router import router as router_roles
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", summary="Основная страница")
 def main_page():
     return {"message": "This is homepage."}
 
 app.include_router(router_users)
 app.include_router(router_orders)
+app.include_router(router_roles)
