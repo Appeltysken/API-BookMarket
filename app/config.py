77e92ba497dff2e178813b3e80a4f3d4f7574db7
Appeltysken <Sorokin6203@yandex.ru>
2024-10-14 17:01:56 +0300

    :wrench: Updated configuration file
    - Added new fields for the secret key and the encryption algorithm for the user's password

--- app/config.py
+++ app/config.py
@@ -8,6 +8,9 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
+    SECRET_KEY: str
+    ALGORITHM: str
+
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
     )
@@ -17,5 +20,11 @@ settings = Settings()
 
 
 def get_db_url():
-    return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
-            f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
+    return (
+        f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
+        f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+    )
+
+    
+def get_auth_data():
+    return {"secret_key": settings.SECRET_KEY, "algorithm": settings.ALGORITHM}
